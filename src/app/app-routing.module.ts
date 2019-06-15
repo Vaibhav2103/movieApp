ba58bfee49b81ef33f19be371b38a4978a5dba09
vaibhav2103 <vaibhav2103@gmail.com>
2019-06-15 17:14:57 +0530

    Display Movie list by calling third party API and Display list, Movie Details

---
+++ src/app/app-routing.module.ts
@@ -0,0 +1,21 @@
+import { NgModule } from "@angular/core";
+import { RouterModule, Routes } from "@angular/router";
+
+const routes: Routes = [
+  { path: "", redirectTo: "movies", pathMatch: "full" },
+  {
+    path: "movies",
+    loadChildren: "./pages/movies/movies.module#MoviesPageModule"
+  },
+  {
+    path: "movies/:id",
+    loadChildren:
+      "./pages/movie-details/movie-details.module#MovieDetailsPageModule"
+  }
+];
+
+@NgModule({
+  imports: [RouterModule.forRoot(routes)],
+  exports: [RouterModule]
+})
+export class AppRoutingModule {}
